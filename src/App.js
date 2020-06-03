966fef2997a3cc17c9f406d42963574fb2eb362c
Jayashree-panda <jayashreepanda12345@gmail.com>
2020-06-03 14:29:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'; 
+import * as fs from 'fs';
+import React,{Component} from 'react'; 
+import { google } from 'googleapis';
+import readline from 'readline';
+class App extends Component { 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	state = { 
 
-export default App;
+	// Initially, no file is selected 
+  selectedFile: null,
+  TOKEN_PATH: 'token.json',
+  SCOPES:['https://www.googleapis.com/auth/drive.metadata.readonly']
+	}; 
+	
+	// On file select (from the pop up) 
+	onFileChange = event => { 
+	
+	// Update the state 
+	this.setState({ selectedFile: event.target.files[0] }); 
+	
+  }; 
+  
+	authorize = (credentials, callback) => {
+    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+  
+    // Check if we have previously stored a token.
+    fs.readFile(this.TOKEN_PATH, (err, token) => {
+      if (err) return this.getAccessToken(oAuth2Client, callback);
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
+  }
+
+  getAccessToken = (oAuth2Client, callback) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: this.SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error('Error retrieving access token', err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(this.TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log('Token stored to', this.TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+      });
+    });
+  }
+  
+  listFiles = (auth) => {
+    const drive = google.drive({version: 'v3', auth});
+    drive.files.list({
+      pageSize: 10,
+      fields: 'nextPageToken, files(id, name)',
+    }, (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const files = res.data.files;
+      if (files.length) {
+        console.log('Files:');
+        files.map((file) => {
+          console.log(`${file.name} (${file.id})`);
+        });
+      } else {
+        console.log('No files found.');
+      }
+    });
+  }
+
+	// On file upload (click the upload button) 
+	onFileUpload = () => { 
+    fs.readFile('credentials.json', (err, content) => {
+      if (err) return console.log('Error loading client secret file:', err);
+      // Authorize a client with credentials, then call the Google Drive API.
+      this.authorize(JSON.parse(content), this.listFiles);
+    });
+    
+	// Create an object of formData 
+	//const formData = new FormData(); 
+	
+	// Update the formData object 
+	// formData.append( 
+	// 	"myFile", 
+	// 	this.state.selectedFile, 
+	// 	this.state.selectedFile.name 
+	// ); 
+	
+	// Details of the uploaded file 
+	//console.log(this.state.selectedFile); 
+	
+	// Request made to the backend api 
+	// Send formData object 
+	//axios.post("api/uploadfile", formData); 
+	}; 
+	
+	// File content to be displayed after 
+	// file upload is complete 
+	fileData = () => { 
+	
+	if (this.state.selectedFile) { 
+		
+		return ( 
+		<div> 
+			<h2>File Details:</h2> 
+			<p>File Name: {this.state.selectedFile.name}</p> 
+			<p>File Type: {this.state.selectedFile.type}</p> 
+			<p> 
+			Last Modified:{" "} 
+			{this.state.selectedFile.lastModifiedDate.toDateString()} 
+			</p> 
+		</div> 
+		); 
+	} else { 
+		return ( 
+		<div> 
+			<br /> 
+			<h4>Choose before Pressing the Upload button</h4> 
+		</div> 
+		); 
+	} 
+	}; 
+	
+	render() { 
+	
+	return ( 
+		<div> 
+			<h1> 
+			GeeksforGeeks 
+			</h1> 
+			<h3> 
+			File Upload using React! 
+			</h3> 
+			<div> 
+				<input type="file" onChange={this.onFileChange} /> 
+				<button onClick={this.onFileUpload}> 
+				Upload! 
+				</button> 
+			</div> 
+		{this.fileData()} 
+		</div> 
+	); 
+	} 
+} 
+
+export default App; 
